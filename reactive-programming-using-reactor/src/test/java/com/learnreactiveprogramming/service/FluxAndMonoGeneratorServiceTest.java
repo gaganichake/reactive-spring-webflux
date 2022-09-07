2a1cef0b2872dec8cba5913de2a39bfbf08164e9
Gagan <gaganichake@yahoo.com>
2022-09-06 21:10:37 -0500

    Testing Flux using JUnit5

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,26 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    void namesFlux() {
+        //given
+
+        //when
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        StepVerifier.create(namesFlux)
+                //.expectNext("Foo", "Bar", "Temp")
+                //.expectNextCount(3)
+                .expectNext("Foo")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
