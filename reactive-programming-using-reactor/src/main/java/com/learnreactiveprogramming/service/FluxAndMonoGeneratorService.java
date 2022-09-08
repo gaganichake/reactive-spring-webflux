06fb4e1cfbd79ea0f667a6453b38c0a5884b3bd8
Gagan <gaganichake@yahoo.com>
2022-09-07 22:01:46 -0500

    Asynchronous Operations using flatMap() Operator, Advanced transform using the concatMap() Operator

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -3,6 +3,7 @@ package com.learnreactiveprogramming.service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 
 public class FluxAndMonoGeneratorService {
@@ -17,14 +18,20 @@ public class FluxAndMonoGeneratorService {
         fluxAndMonoGeneratorService.nameMono()
                 .subscribe(name -> System.out.println("Mono Name is " + name));
 
-        fluxAndMonoGeneratorService.namesFluxTransform()
-                .subscribe(name -> System.out.println("Flux Transformed Name is " + name));
+        fluxAndMonoGeneratorService.namesFluxMap()
+                .subscribe(name -> System.out.println("Flux map Name is " + name));
 
         fluxAndMonoGeneratorService.namesFluxImmutable()
-                .subscribe(name -> System.out.println("Flux Immutable Name is " + name));
+                .subscribe(name -> System.out.println("Flux immutable Name is " + name));
 
         fluxAndMonoGeneratorService.namesFluxFilter(3)
-                .subscribe(name -> System.out.println("Flux  Name is " + name));
+                .subscribe(name -> System.out.println("Flux filtered Name is " + name));
+
+        fluxAndMonoGeneratorService.namesFluxFlatMap()
+                .subscribe(name -> System.out.println("Flux flat map Name char is " + name));
+
+        fluxAndMonoGeneratorService.namesFluxFlatMapAsync()
+                .subscribe(name -> System.out.println("Flux Async Name char is " + name));
     }
 
     public Flux<String> namesFlux() {
@@ -37,10 +44,10 @@ public class FluxAndMonoGeneratorService {
                 .log();
     }
 
-    public Flux<String> namesFluxTransform() {
+    public Flux<String> namesFluxMap() {
         return Flux.fromIterable(List.of("Foo", "Bar", "Temp"))
+                //.map(s -> s.toUpperCase())
                 .map(String::toUpperCase)
-//                .map(s -> toString().toUpperCase())
                 .log();
     }
 
@@ -56,16 +63,36 @@ public class FluxAndMonoGeneratorService {
                 .filter(s -> s.length() > nameLength);
     }
 
-    public Flux<String> namesFluxTransformFlatMap() {
+    public Flux<String> namesFluxFlatMap() {
         return Flux.fromIterable(List.of("Foo", "Bar", "Temp"))
                 .map(String::toUpperCase)
-//                .flatMap(s -> splitString(s))
-                .flatMap(this::splitString)
-                .log();
+                //.flatMap(s -> splitString(s))
+                .flatMap(this::splitString);
     }
 
     private Flux<String> splitString(String name) {
         String[] split = name.split("");
         return Flux.fromArray(split);
+    }
+
+    public Flux<String> namesFluxFlatMapAsync() {
+        return Flux.fromIterable(List.of("Foo", "Bar", "Temp"))
+                .map(String::toUpperCase)
+                .flatMap(this::splitStringWithDelay)
+                .log();
+    }
+
+    private Flux<String> splitStringWithDelay(String name) {
+        String[] split = name.split("");
+        long delay = 1000;
+        return Flux.fromArray(split)
+                .delayElements(Duration.ofMillis(delay));
+    }
+
+    public Flux<String> namesFluxConcatMap() {
+        return Flux.fromIterable(List.of("Foo", "Bar", "Temp"))
+                .map(String::toUpperCase)
+                .concatMap(this::splitStringWithDelay)
+                .log();
     }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -29,7 +29,7 @@ class FluxAndMonoGeneratorServiceTest {
         //given
 
         //when
-        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxTransform();
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxMap();
 
         //then
         StepVerifier.create(namesFlux)
@@ -65,15 +65,43 @@ class FluxAndMonoGeneratorServiceTest {
     }
 
     @Test
-    void namesFluxTransformFlatMap() {
+    void namesFluxFlatMap() {
         //given
 
         //when
-        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxTransformFlatMap();
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxFlatMap();
 
         //then
         StepVerifier.create(namesFlux)
                 .expectNext("F", "O", "O", "B", "A", "R", "T", "E", "M", "P")
                 .verifyComplete();
+    }
+
+    @Test
+    void namesFluxFlatMapAsync() {
+        //given
+
+        //when
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxFlatMapAsync();
+
+        //then
+        StepVerifier.create(namesFlux)
+                // .expectNext("F", "O", "O", "B", "A", "R", "T", "E", "M", "P") // Ordering cannot be expected with time delay
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxConcatMap() {
+        //given
+
+        //when
+        Flux<String> namesFlux = fluxAndMonoGeneratorService.namesFluxConcatMap();
+
+        //then
+        StepVerifier.create(namesFlux)
+                .expectNext("F", "O", "O", "B", "A", "R", "T", "E", "M", "P")
+                .verifyComplete();
+        // Ordering is maintained even with time delay by waiting for the previous operation to complete.
     }
 }
